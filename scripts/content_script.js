d267fae91b2bf256dc2c9d1c9e4d389979b68795
Woonyung <woonyungchoi@gmail.com>
2014-11-11 19:39:05 -0500

    added function for searching inbox contents

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -3,10 +3,12 @@ Appropriating Interaction Technology
 with Woon and MT :D
 */
 
+var value;
+
 //------------------DOC READY-------------------//
 $("document").ready(function(){
 
-	console.log("Running from extension");
+  console.log("------------ Running from extension ---------- ");
     
     //change cursor
     // $("body").css("cursor", "url('"+chrome.extension.getURL('glitter_cursor.gif')+"'), default");
@@ -14,19 +16,30 @@ $("document").ready(function(){
     // make new div element and assign id
     var emoji = document.createElement("div");
     $(emoji).attr("id", "emoji")
-    		.html("<img src='" + chrome.extension.getURL('images/icon.png')+"'>");
+        .html("<img src='" + chrome.extension.getURL('images/icon.png')+"'>");
 
     // insert into body as first child
-   	document.body.insertBefore(emoji, document.body.firstChild);
+    document.body.insertBefore(emoji, document.body.firstChild);
 
+    // checking words every 5 seconds
+    setInterval(searchingWords, 5000);
 
-   	// searching for the specific words..
-   	// it has to be run everytime user clicked new inbox item...
-   	var value = $('body').html(); 
-   	if (value.indexOf('goofy') > -1){
-   		console.log("I found the word");
-   	} else {
-   		console.log("I found nothing");
-   	}
-
+    ////
+    // when are we going to clear those out?...
+    clearInterval();
 });
+
+function searchingWords(){
+  // searching for the specific words
+
+  value = $('.ii').html(); // get value from gmail contents
+  if( value !== undefined ){ // if a user enter to inbox
+    if (value.indexOf('oops') > -1){
+      console.log("I found the word");
+      $('.ii').append("<img src='" + chrome.extension.getURL('images/icon.png')+"'>");
+    } else {
+      console.log("I found nothing");
+    }
+  } else { console.log("You are not getting there"); }
+
+}
