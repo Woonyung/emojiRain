4018ca98187529668a92f7de74da8dfa386e765b
Woonyung <woonyungchoi@gmail.com>
2014-11-13 06:19:35 -0500

    clear out the code

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -2,53 +2,48 @@
 Appropriating Interaction Technology
 with Woon and MT :D
 
-
-1. how can we store variable for imageURL? -- so that we can manipulate multiple emojis
-	chrome.extension.getURL('images/icon.png') - doesn't work...
-
-2. function ---> how can we clear out and call everytime..?
-
+1. function ---> how can we clear out and call everytime..?
+2. how can we raining emoji on the TOP OF THE BROWSER?
 3. how can we spread emoji to whole document
 
 */
 
-
 var value;
+
+// keywords - that is going to be matched with 
+var keywords = ['happy', 'christmas'];
 
 //------------------DOC READY-------------------//
 $("document").ready(function(){
 
-  console.log("------------ Running from extension ---------- ");
-    
-  // checking words every 5 seconds
-  setInterval(searchingWords, 10000);
- // setTimeout(searchingWords, 5000);
-
-  ////
-  // when are we going to clear those out?...
-  //clearInterval();
+	console.log("------------ Running from extension ---------- ");
+	setInterval(searchingWords, 5000);
 });
 
-
 function searchingWords(){
-	// searching for the specific words
-	value = $('.ii').html(); // get value from gmail contents
+	// look for the value from gmail contents
+	value = $('.ii').text().toLowerCase();
+	//console.log(value);
+
 	if( value !== undefined ){ // if a user enter to inbox
-		if (value.indexOf('birthday') > -1){
-			console.log("I found the word");
+		for (var i = 0; i < keywords.length; i++){
+			// if it contains specific words
+			if ( value.indexOf(keywords[i]) > -1){ 
+				console.log("I found word " + keywords[i]);
 
-			// do something inside of the inbox 
-			$('.nH').append("<div class='fallingOff'></div>");
-			// $('.fallingOff').append("<div class='emojis'><span></span></div>");
-			
-			var imgURL = chrome.extension.getURL("images/icon1.png"); 
-			$('.fallingOff').append("<div class='emojis'><span style='background:url("+ imgURL +")'></span></div>");
-
-		} else {
-			console.log("I found nothing");
-		}
+				// run function 
+				rainEmoji(keywords[i]);
+			} 
+		} 
 	} else { 
 		console.log("You are not getting there"); 
 	}
+}
 
+function rainEmoji(keyword){
+	// getting URL - from images folder
+	var imgURL = chrome.extension.getURL("images/icon_" + keyword + ".png"); 
+	// append to div - for css animation
+	$('.nH').append("<div class='emojis'><span style='background:url("+ imgURL +")'></span></div>");
+	
 }
