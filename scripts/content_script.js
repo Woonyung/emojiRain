472acb9c7a79f9ccb50e98ac3c8b73de1ff455df
Woonyung <woonyungchoi@gmail.com>
2014-11-21 14:39:40 -0500

    fixed code to work with facebook chat

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -16,71 +16,101 @@ var valueForFacebook;
 var valueForGchat;
 
 var word;
-
+var intervalID  = 0;
 // keywords - that is going to be matched with 
 var keywords = ['happy', 'christmas'];
+
+//{ 'data-reactid':length of chats }
+var chatIDs = {};
+
 
 //------------------DOC READY-------------------//
 $("document").ready(function(){
 
 	console.log("------------ Running from extension ---------- ");
 	setTimeout(attachListeners,100);
+
+
 });
 
 
 function attachListeners(){
 	console.log("----attachListeners is running----");
 	// setTimeout(searchingWords, 2000);
-	setInterval(searchingWords, 2000);
+
+	intervalID = setInterval(searchingWords, 2000);
+
+
 }
-
-
 
 function searchingWords(){
 	// clear out the emojis
 	$('.emojis').remove();
 
-	// look for the value from facebook chat contents
-	valueForFacebook = $('.conversation').text().toLowerCase();
-	console.log(valueForFacebook);
+	// checking all conversation class's children
+	var allConvesation = $('.conversation').children();
+	
+	//looping through chat windows
+	for (var i = 0; i < allConvesation.length; i++) {
+		// console.log(allConvesation[i].getAttribute('data-reactid')); // <div data-reactid='1.f'>...</div>
 
+		var currentID = allConvesation[i].getAttribute('data-reactid');
+		var numMessages = allConvesation[i].children.length;
+		// get the newsest message
+		var newestMessage = allConvesation[i].children[numMessages-1].innerText;
+		
+		// is this a new chat
+		if(currentID in chatIDs){
 
-	// split with am or pm (timestamp) or space OR
-	// ! mark , ? OR
-	// white space ( space )
-	var regex = /am|pm|\s|!{1,}|\?{1,}|,|ago/gi 
-	word = valueForFacebook.split(regex);
-	console.log(word);
+			// we already have this object so now need to check the num of messages
+			// console.log("More conversation in chat : " + currentID);
+			if(numMessages > chatIDs[currentID]){
+				console.log("More conversation in chat : " + newestMessage);
+				chatIDs[currentID] = numMessages;
+				var words = newestMessage.split(' ');
+				
+				words[words.length -1].replace(/\n|\r+/g, '');
+				console.log(words);
+				
+				if($.inArray('happy', words) !== -1){
+					rainEmoji('happy',currentID);
+				}
 
-
-	if( word !== undefined ){ 
-		for (var i = 0; i < keywords.length; i++){
-			// if it contains specific words
-			if ( word.indexOf(keywords[i]) > -1){ 
-				console.log("I found word " + keywords[i]);
-
-				// run function 
-				rainEmoji(keywords[i]);
-			} 
-		} 
-	} else { 
-		console.log("You are not getting there"); 
-	}
+			}
+			// else{
+			// 	console.log("No new conversation");
+			// 	console.log(chatIDs[currentID]+" : "+numMessages);
+			// }
+		}
+		else{
+			//This is what happens when a new chat is started
+			// New Object!! Lets add it to the object
+			chatIDs[currentID] = numMessages ;
+			console.log("Chat added!");
+			console.log(chatIDs);
+			
+		}
+	};
 }
 
-function rainEmoji(keyword){
+function rainEmoji(keyword,id){
 	console.log(keyword);
 	// getting URL - from images folder
 	var imgURL = chrome.extension.getURL("images/icon_" + keyword + ".png"); 
 	// append to div - for css animation
 	console.log(imgURL);
+	console.log($("[data-reactid="+'\"'+id+'\"'+"]"));
 
+	
+	// $(id).append("<div style='margin-left:" + 100 + "px; margin-top:"+ 100 + "px;' class='emojis " + keyword + "'><span style='background:url("+ imgURL +")'></span></div>");
 	// Randomly assign value 
 	for (var i = 0; i < 15; i++){
 		var x = Math.round(Math.random() * 900)-20;
 		var y = Math.round(Math.random() * 10);
-		$('.conversation').append("<div style='margin-left:" + x + "px; margin-top:"+ y + "px;' class='emojis " + keyword + "'><span style='background:url("+ imgURL +")'></span></div>");
-		// $('.h7').prepend("<div style='margin-left:" + x + "px;' class='emojis " + keyword + "'><span style='background:url("+ imgURL +")'></span></div>");
+		// $('.conversation').append("<div style='margin-left:" + x + "px; margin-top:"+ y + "px;' class='emojis " + keyword + "'><span style='background:url("+ imgURL +")'></span></div>");
+		$("[data-reactid="+'\"'+id+'\"'+"]").append("<div style='margin-left:" 
+													+ x + "px; margin-top:"+ y + "px;' class='emojis "
+													+ keyword + "'><span style='background:url("
+													+ imgURL +")'></span></div>");
 	}
-	// $('.nH').append("<div class='emojis " + keyword + "'><span style='background:url("+ imgURL +")'></span></div>");
 }
